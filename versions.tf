10ff484336b2c7c6e44ea1c3e3b289f8fa530ee8
Bruno Schaatsbergen <git@bschaatsbergen.com>
2021-12-18 21:33:42 +0100

    Wrap `configuration_aliases` in `aws` provider

--- versions.tf
+++ versions.tf
@@ -2,8 +2,11 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-    aws = ">= 3.66"
+     aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.39.0"
 
-    configuration_aliases = ["aws.replica"]
+      configuration_aliases = [aws.replica]
+    }
   }
 }
