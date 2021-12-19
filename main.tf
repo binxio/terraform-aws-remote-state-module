aff14baceacbda3cb750e3611018d56d4f3a80a7
Bruno Schaatsbergen <git@bschaatsbergen.com>
2021-12-19 19:59:18 +0100

    Remove S3 object lock

--- main.tf
+++ main.tf
@@ -52,11 +52,6 @@ resource "aws_s3_bucket" "remote_state_bucket" {
       }
     }
   }
-
-  object_lock_configuration {
-    object_lock_enabled = "Enabled"
-  }
-
   logging {
     target_bucket = aws_s3_bucket.log_bucket.id
     target_prefix = "${var.bucket_name}/"
